04348bd2f031eec43d76ee09708eeeae1ea8db96
daschne8 <daschne8@gmail.com>
2019-05-03 14:23:55 -0400

    fixed bug where multiples of information would be listed

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -3,6 +3,7 @@ class GenreController < ApplicationController
   get '/genres/:slug' do
     @current_user = Helpers.current_user(session)
     @genre = Genre.find_by_slug(params[:slug])
+    @games = @genre.games
     erb :'/genres/show'
   end
 
